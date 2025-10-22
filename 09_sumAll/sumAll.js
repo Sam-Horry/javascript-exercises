885ac5dc389291b44abd99488d2df3bedab250e9
Samuel Horry <211251918+Sam-Horry@users.noreply.github.com>
2025-10-22 16:32:05 +0100

    arrays and loops
    completed the arrays and loops exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) return "ERROR";
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+  if (x > y) {
+    const temp = x;
+    x = y;
+    y = temp;
+  }
+  var numbers = [];
 
+  for (var i = x; i <= y; i++) {
+    numbers.push(i);
+  }
+  return numbers.reduce((acc, curr) => acc + curr);
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  var converted = (num - 32) * 5 / 9;
+  converted = Math.round(converted * 10) / 10;
+  return converted;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  var converted = num * 9 / 5 + 32;
+  converted = Math.round(converted * 10) / 10;
+  return converted;
 };
 
 // Do not edit below this line
